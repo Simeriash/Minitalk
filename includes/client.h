666298c128b0ef5a424fbd892edb7ba722bd974b
julauren <249057325+Simeriash@users.noreply.github.com>
2025-12-15 11:01:10 +0100

    init

---
+++ includes/client.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   client.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 09:35:26 by julauren          #+#    #+#             */
+/*   Updated: 2025/12/15 10:55:28 by julauren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef CLIENT_H
+# define CLIENT_H
+
+# include <unistd.h>
+# include <signal.h>
+# include "../src/libft/libft.h"
+
+#endif
---
+++ includes/server.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   server.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: julauren <julauren@student.42angouleme.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/15 09:37:01 by julauren          #+#    #+#             */
+/*   Updated: 2025/12/15 10:55:26 by julauren         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SERVER_H
+# define SERVER_H
+
+# include <unistd.h>
+# include <signal.h>
+# include "../src/libft/libft.h"
+
+#endif
